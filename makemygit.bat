ea1a759de62331ee2525bccc82e26334886823e0
Your Name <you.example.com>
2016-05-02 22:48:33 +0200

    Signed-off-by: Your Name <you.example.com>

---
+++ makemygit.bat
@@ -0,0 +1,6 @@
+echo "# vlwe-jq030i012-jQHelper" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/itbas/vlwe-jq030i012-jQHelper.git
+git push -u origin master
